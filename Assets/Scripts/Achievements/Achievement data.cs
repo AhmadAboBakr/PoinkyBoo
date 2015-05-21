2252df364110b8a5b3e4b846a3d44ff94f70a13a
unknown <alaa.mohammed.hatata@gmail.com>
2015-05-21 19:30:50 +0200

    chievments start

---
+++ Assets/Scripts/Achievements/Achievement data.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Achievementdata {
+    //public int Value;
+    public string ID;
+    public bool Unlocked;
+	
+    public Achievementdata (string id,bool unlocked)
+    {
+        ID = id;
+        Unlocked = unlocked;
+    }
+}
--- Assets/Scripts/Achievements/Achievements Handler.cs
+++ Assets/Scripts/Achievements/Achievements Handler.cs
@@ -2,23 +2,46 @@
 using System.Collections;
 using GooglePlayGames;
 using UnityEngine.SocialPlatforms;
+using System.Collections.Generic;
 
 public class AchievementsHandler : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-    void AchievementUnlocked (string avhievementID)
+    public static AchievementsHandler instance;
+    public Dictionary<int, Achievementdata> Achievements;
+    void Awake()
     {
-        Social.ReportProgress(avhievementID, 100.0f, (bool success) =>
+        if (!AchievementsHandler.instance)
+            AchievementsHandler.instance = this;
+        GameManager.Move += Move;
+        GameManager.clear += Clear;
+    }
+	public void Move()
+    {
+        //if()
+        //{
+
+        //}
+        //else if()
+        //{
+
+        //}
+    }
+    public void Clear ()
+    {
+
+    }
+    void AchievementUnlocked (string avhievementID,double value)
+    {
+        Social.ReportProgress(avhievementID, value, (bool success) =>
         {
             // handle success or failure
         });
+    }
+    void Start()
+    {
+        Achievements=new Dictionary<int,Achievementdata>();
+        Achievements.Add(10, new  Achievementdata("id1", false));
+        Achievements.Add(20, new Achievementdata("id2", false));
+        Achievements.Add(50, new Achievementdata("id3", false));
+        Achievements.Add(100, new Achievementdata("id4", false));
     }
 }
